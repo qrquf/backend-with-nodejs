884514d11eb6afe8ed1186e8c0c64e8c17f6e118
qrquf <siddharthsrivastava117@gmail.com>
2025-01-09 05:59:36 -0800

    updated user routes

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ require("dotenv").config();
 const express=require('express');
 const app=express();
 const cors=require('cors');
-app.use(cors);
-app.listen(process.env.PORT,()=>{
+
+app.listen(process.env.PORT,'0.0.0.0',()=>{
     console.log("the server has started listening");
 });
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const routes=require('./routes/index.js');
