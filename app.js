9a28e41108ba43ecf06d57dd738197feb84fd7f0
qrquf <siddharthsrivastava117@gmail.com>
2025-01-10 04:38:29 -0800

    made upload file static

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express=require('express');
 const app=express();
 const cors=require('cors');
+const path=require('path');
 
 app.listen(process.env.PORT,()=>{
     console.log("the server has started listening");
@@ -9,5 +10,6 @@ app.listen(process.env.PORT,()=>{
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 const routes=require('./routes/index.js');
 app.use('/',routes);
