9d0f15b82dc9a24e2159af508722ceb127e88945
qrquf <siddharthsrivastava117@gmail.com>
2025-03-07 05:01:23 -0800

    modified subscription schema

--- models/subscription.js
+++ models/subscription.js
@@ -3,7 +3,7 @@ const schema=mongoose.Schema({
     Seller_id:{type:String},
     subscritpion_id:{type:String},
     Start_date:{type:Date},
-    Expiry_Date:{type:Date},
+    Expiry_date:{type:Date},
     Plan_name:{type:String},
     Subs_value:{type:String},
 },{collection:'subscription'});
